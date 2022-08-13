4230fbd811df992196ff8371dce3e48a23e224ae
Angelina <angelinarwhite@gmail.com>
2022-08-12 22:48:57 -0400

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) 
+{
+    let rev = "";
+    for (let i=str.length-1; i>=0; i--)
+    {
+        rev = rev + str[i];
+    }
+    
+    return str === rev;
+}
 
 module.exports = palindrome;
