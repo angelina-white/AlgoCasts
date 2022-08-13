e7d3836a5f00a75b639936203471c7da3651bfdd
Angelina <angelinarwhite@gmail.com>
2022-08-12 23:14:25 -0400

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) 
+{
+    let str = n.toString();
+    let rev = "";
+
+    for (let i = str.length - 1; i>=0; i--)
+    {
+        rev = rev + str[i];
+    }
+
+        return parseInt(rev) * Math.sign(n);
+}
 
 module.exports = reverseInt;
